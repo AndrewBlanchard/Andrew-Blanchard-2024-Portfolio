bd464ddccf00da616f7cbc9b0df986596f24b730
AndrewBlanchard <blanchard0andrew@gmail.com>
2024-09-03 18:53:03 -0500

    Created header and footer jsx files.

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <Link to="/">About Me</Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/resume">Resume</Link>
+      </nav>
+    </header>
+  );
+}
